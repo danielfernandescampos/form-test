f102751d4947d2465d8e75d1491822e4ffaf9d61
Daniel <ubuntu@DESKTOP-9314L0B.localdomain>
2021-04-15 14:44:45 -0300

    initial commit: add main components, configure routing, add home html content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { SubmitPageComponent } from './submit-page/submit-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'submit', component: SubmitPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SubmitPageComponent } from './submit-page/submit-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SubmitPageComponent
   ],
   imports: [
     BrowserModule,
